a3f8818d870655aa92d0dae0d609bef567a0085a
smilegupta <guptamiley3012@gmail.com>
2021-04-04 23:16:38 +0530

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import ConfettiCanvas from "react-confetti-canvas";
 
-function App() {
+const App = () => {
+  const [Confetti, setConfetti] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {!Confetti && (
+        <div className="container  d-flex justify-content-center mt-5">
+          {" "}
+          <button className="btn btn-dark" onClick={() => setConfetti(true)}>
+            {" "}
+            Click ME
+          </button>{" "}
+        </div>
+      )}
+      {Confetti === true && (
+        <div style={{ width: "100%", height: "100vh" }}>
+          <ConfettiCanvas paperCount={300} ribbonCount={0} />
+        </div>
+      )}
+    </Fragment>
   );
-}
+};
 
 export default App;
+
+
+// For More Refernce: https://github.com/fufu70/react-confetti-canvas
